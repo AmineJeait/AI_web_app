f4f6e3213fc5004c3783adbc99c69102cd63ddb7
AmineJeait <156703155+AmineJeait@users.noreply.github.com>
2025-07-23 08:47:24 +0100

    Update test.py

--- chat_bot_web_app_v0.1/server/test.py
+++ chat_bot_web_app_v0.1/server/test.py
@@ -1,20 +1,56 @@
 import requests
+from flask import Flask,request,jsonify
+from flask_cors import CORS
 
+
+
+app = Flask(__name__)
+
+url = "https://e96baf93463f.ngrok-free.app/message"
+CORS(app, origins=["http://localhost:5173"])
+
+
+
+@app.route('/',methods = ['POST'])
+def proxy():
+    data = {
+        "message": ""
+    }
+
+    userinput = request.get_json()
+    data["message"] = userinput
+    print(userinput)
+    response = requests.post(url,json=userinput)
+
+    print(response.status_code)  # Check if it's 200
+    print(response)         # See what the server actually returned
+
+    if response.status_code == 200:
+        try:
+            data = response.json()
+            print(data)
+        except ValueError:
+            print("Response is not valid JSON.")
+    else:
+        print(f"Error: Status code {response.status_code}")
+
+
+    return jsonify(data)
 # Your Flask endpoint URL
-url = "http://127.0.0.1:5000/message"
+
 
 # Sample data to send
-data = {
-    "message": "Hello Ollama, can you assist me?"
-}
+
 
 # Send POST request with JSON data
-response = requests.post(url, json=data)
 
-# Check response
-if response.ok:
-    print("Response JSON from Flask API:")
-    print(response.json())  # This should show {"reply": "..."}
-else:
-    print(f"Failed with status {response.status_code}")
-    print(response.text)
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
